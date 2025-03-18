2088e98555392023d24c17b7afb7146a1d0c24e8
SebastianEncinas <sebastian.encinas@sagradoalcal.edu.ar>
2025-03-18 16:13:38 -0300

    Ejercicio3

---
+++ ejercicio3.py
@@ -0,0 +1,8 @@
+temperatura= int(input("cual es  la temperatura?"))
+
+if temperatura > 30:
+    print("hace calor")
+elif temperatura  >10 < 30:
+    print("templado")
+else:
+    print("hace frio")
